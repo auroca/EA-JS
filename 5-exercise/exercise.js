8c778fce64d0e9a79f0b819547182613a4895b75
auroca <aurora.calderon@estudiantat.upc.edu>
2026-02-19 19:07:52 +0100

    ejercicio completado

--- 5-exercise/exercise.js
+++ 5-exercise/exercise.js
@@ -11,9 +11,51 @@ fetch('https://jsonplaceholder.typicode.com/users/')
       // YOUR CODE STARTS HERE
       console.log("--- Processed Users ---");
       // 1. Filter even IDs
+
+      const evenIds = users.filter((user) => (user.id%2) == 0);
+      console.log(evenIds);
+
       // 2. Map to clean objects {id, name, city}
+
+      const transform = users.map((user) => ({
+        id: `${user.id}`,
+        name: `${user.name}`,
+        city: `${user.address.city}`,
+      }));
+      console.log(transform);
+
       // 3. Add Guest User at the start using Spread (...)
+
+      const guestUser = {
+          "id": 0,
+          "name": "Guest User",
+          "username": "guest",
+          "email": "guest@april.biz",
+          "address": {
+            "street": "Barcelona street",
+            "suite": "Apt. 200",
+            "city": "Barcelona",
+            "zipcode": "00000",
+            "geo": {
+              "lat": "-1",
+              "lng": "2"
+            }
+          },
+          "phone": "111111111",
+          "website": "website.org",
+          "company": {
+            "name": "Company",
+            "catchPhrase": "catchphrase",
+            "bs": "qwertyuiop"
+          }
+      }
+      const spread = [guestUser, ...users]
+      console.log(spread);
       
       console.log("--- Statistics ---");
       // 4. Reduce to count total characters in names
+
+      const reduce = users.reduce((total, user) => total + user.username.length, 0);
+      console.log(reduce);
+
   });
